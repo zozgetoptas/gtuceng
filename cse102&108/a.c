98cc5ef6629d02a001b1d65d176247fa3ae7fe69
zeynepozgetoptas <zeyneppozgeetoptass@gmail.com>
2026-09-06 11:38:51 +0300

    done may be

--- cse102&108/a.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int x = 10;
-    int *p = &x;
-    *p = 20;
-    printf("%d\n", *p);
-    return 0;
-}
--- cse102&108/midterm_cikmis_13.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-void compress(char *input, char *output)
-{
-
-}
